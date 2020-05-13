56c486c222bb125c7fc7c59f96ac35055fdbe019
PKQ <zhutaonan@i-i.ai>
2020-05-13 10:51:35 +0800

    update

---
+++ gaussian_detection/data/data_aug/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @author :adolf
+from .iaa_augment import IaaAugment
+from .augment import *
+from .random_crop_data import EastRandomCropData, PSERandomCrop
---
+++ gaussian_detection/data/data_utils/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+# @author :adolf
+from .clockwise_points import order_points_clockwise
